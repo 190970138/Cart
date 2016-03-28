80b61af472c6777dfced4a726f3bda54e0404ad1
hnrpf <fu.wei.2000@163.com>
2016-03-28 15:27:02 +0800

    Update CartController.java
    controller层修改

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -38,14 +38,22 @@ public class CartController extends AbstractCtl {
         try {
             //获取请求的IP,记录日志
             String remoteIp = HttpUtils.getRemoteIp(request);
-            logger.info("CartController.getCart() | remoteIp=" + remoteIp);
+            logger.info("CartController.getCart() | remoteIp=" + remoteIp);     //这种普通的查询日志，和下面异常中的日志最好不要放一起
+                                                                                //不然查询那些异常的特别慢
             //获取前端请求参数并校验
             Map<String, String> paramMap = HttpUtils.getParameterMap(request);
             logger.info("CartController.getCart() | paramMap=" + paramMap);
             if (paramMap == null || paramMap.size() == 0) {
-                throw new IllegalArgumentException("query param is null");
+                throw new IllegalArgumentException("query param is null");      //你这抛出异常有人处理么，你自己要处理controller层的异常
+                                                                                //我习惯定义接口的返回数据如下：
+                                                                                //retCode	整数	响应码	200：操作成功
+                                                                                                            500：系统异常
+                                                                                                            401：请求参数错误，请稍后重试	是
+                                                                                //retDesc	字符串	响应描述		是
+                                                                                //retData	Map	响应数据		
+
             }
-            result = cartService.getCart(paramMap.get("cartId"));
+            result = cartService.getCart(paramMap.get("cartId"));               //此处最好要把参数取出来，做检查，如判空、是否整型等
             resultStr = JsonUtil.toJson(result);
             logger.info("CartController.getCart() | result:" + resultStr);
             response.getOutputStream().write(resultStr.getBytes());
